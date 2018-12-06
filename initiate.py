11e33f2970b3645b7a296742b8b538e64f5a21b6
arungirdhar2 <myecrowsoft@gmail.com>
2018-12-06 23:26:27 +0530

    Add files via upload
    updates

---
+++ initiate.py
@@ -0,0 +1,28 @@
+import unittest
+import data as d
+import sendEmail
+from fabric import Connection
+
+
+class CommandTests(unittest.TestCase):
+    processes = []
+
+    def tearDown(self):
+        for p in self.processes:
+            if p.poll() is None:
+                p.terminate()
+
+    def test_if_top_command_is_running(self):
+        """
+        :return:
+        """
+        c = Connection(d.server, user=d.username, connect_timeout=600, connect_kwargs={'password': d.password})
+        result = c.run(d.command)
+        print(result)
+        self.assertEqual(result.stdout.strip(), d.verification)
+        c.close()
+        sendEmail.main("From Initiate : ",result)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ web.py
@@ -0,0 +1,47 @@
+import unittest
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import data as d
+import sendEmail
+
+chrome_path = os.path.abspath("chromedriver")
+
+driver = webdriver.Chrome(chrome_path)
+
+driver.maximize_window()
+
+driver.implicitly_wait(120)
+driver.get('http://'+d.server+':'+d.port+'/')
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "recnum"))
+    )
+    element.send_keys(d.recordNumber)
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "vertag"))
+    )
+    element.send_keys(d.taxVersion)
+
+
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "button"))
+    )
+    element.click()
+
+    element = WebDriverWait(driver, 200).until(
+        EC.presence_of_element_located((By.XPATH, "//div[@class='jumbotron']//tbody//tr[1]"))
+    )
+    elements = driver.find_elements_by_xpath("//div[@class='jumbotron']//tbody//tr")
+    print(len(elements))
+
+    sendEmail.main("From Web : ",str(len(elements)))
+
+finally:
+    driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
