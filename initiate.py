3e452bcfedcdee81056907051a7ad6abac87a9a9
arun <myecrowsoft@gmail.com>
2018-12-15 22:45:10 +0530

    updated error mess and command in data

--- initiate.py
+++ initiate.py
@@ -3,9 +3,10 @@ import data as d
 import sendEmail
 from fabric import Connection
 
+
 class CommandTests(unittest.TestCase):
     processes = []
-
+    msg =""
     def tearDown(self):
         for p in self.processes:
             if p.poll() is None:
@@ -15,12 +16,18 @@ class CommandTests(unittest.TestCase):
         """
         :return:
         """
-        c = Connection(d.server, user=d.username, connect_timeout=600, connect_kwargs={'password': d.password})
-        result = c.run(d.command)
-        print(result)
-        #self.assertEqual(result.stdout.strip(), d.verification)
-        c.close()
-        sendEmail.main("From Initiate : ",str(result))
+        try:
+            c = Connection(d.server, user=d.username, connect_timeout=600, connect_kwargs={'password': d.password})
+            res = c.run(d.command)
+            verify = c.run("echo $?")
+            print(str(verify)+"---")
+            self.assertEqual(verify.stdout.strip(), "0")
+            msg = str(res.stdout)
+        except Exception as e:
+            msg=str(e.result)
+        finally:
+            c.close()
+            sendEmail.main("From Initiate : ",str(msg))
 
 
 if __name__ == '__main__':
