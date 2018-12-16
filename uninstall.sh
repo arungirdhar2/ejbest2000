86e73116b9e56ab3846d3f704a4324b64ff653e6
arungirdhar2 <myecrowsoft@gmail.com>
2018-12-16 23:08:09 +0530

    Add files via upload
    cache clear

--- uninstall.sh
+++ uninstall.sh
@@ -29,16 +29,16 @@ assert ()                 #  If condition false,
   #   and continue executing the script.
   fi
 
-  if [ ! $lastVal = 1 ]
-  then
-    echo $1
-    echo "Cannot uninstall requirement fabric, not installed:  \"$1\""
-    echo "File \"$0\", line $lineno"    # Give name of file and line number.
-    exit $E_ASSERT_FAILED
-  # else
-  #   return
-  #   and continue executing the script.
-  fi
+#  if [ ! $lastVal = 1 ]
+#  then
+#    echo $1
+#    echo "Cannot uninstall requirement , already uninstalled:  \"$1\""
+#    echo "File \"$0\", line $lineno"    # Give name of file and line number.
+#
+#  # else
+#  #   return
+#  #   and continue executing the script.
+#  fi
 
 } # Insert a similar assert() function into a script you need to debug.    
 #######################################################################
@@ -46,20 +46,10 @@ assert ()                 #  If condition false,
 #assert "sudo apt --assume-yes remove python3-pip" $LINENO
 pip3 uninstall fabric
 pip3 uninstall selenium
-sudo rm -rf temp
-sudo apt --assume-yes remove python3-pip
-#sudo apt-get update
-#assert "sudo apt-get --assume-yes install -y libappindicator1 fonts-liberation" $LINENO
-#assert "mkdir temp" $LINENO
-#assert "cd temp" $LINENO
-#assert "wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" $LINENO
-#sudo dpkg -i google-chrome*.deb
-#assert "sudo apt-get -f --assume-yes install" $LINENO
-#assert "sudo dpkg --configure -a" $LINENO
-#assert "cd .." $LINENO
-#assert "sudo chmod +x chromedriver" $LINENO
-#assert "sudo rm -rf temp" $LINENO
+assert "sudo rm -rf temp" $LINENO
+assert "sudo rm -rf __pycache__"
+
  The remainder of the script executes only if the "assert" does not fail.
 
-echo "This statement echoes only if the \"assert\" does not fail."
+echo "HURRAY !! This statement echoes only if the \"assert\" does not fail."
 exit $?
