a3acd459c96680b3f312827be8b15dc653328ea4
ARUN <arungirdhar@qainfotech.com>
2019-01-08 12:48:43 +0530

    verify

--- initiate.py
+++ initiate.py
@@ -2,11 +2,17 @@ import unittest
 import data as d
 import sendEmail
 from fabric import Connection
+import sys
 
 
 class CommandTests(unittest.TestCase):
     processes = []
-    msg =""
+    server = d.server
+    username = d.username
+    password = d.password
+    command = d.command
+    msg = ""
+
     def tearDown(self):
         for p in self.processes:
             if p.poll() is None:
@@ -17,18 +23,27 @@ class CommandTests(unittest.TestCase):
         :return:
         """
         try:
-            c = Connection(d.server, user=d.username, connect_timeout=600, connect_kwargs={'password': d.password})
-            res = c.run(d.command)
+            print('server', self.server)
+            print('username', self.username)
+            print('password', self.password)
+            c = Connection(self.server, user=d.username, connect_timeout=600, connect_kwargs={'password': self.password})
+            res = c.run(self.command)
             verify = c.run("echo $?")
             print(str(verify)+"---")
             self.assertEqual(verify.stdout.strip(), "0")
-            msg = str(res.stdout)
+            self.msg = str(res.stdout)
         except Exception as e:
-            msg=str(e.result)
+            self.msg=str(e.result)
+
         finally:
             c.close()
-            sendEmail.main("From Initiate : ",str(msg))
+            sendEmail.main("From Initiate : ", str(self.msg))
 
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        CommandTests.server = sys.argv.pop()
+        CommandTests.password = sys.argv.pop()
+        CommandTests.username = sys.argv.pop()
+        CommandTests.command = sys.argv.pop()
     unittest.main()
