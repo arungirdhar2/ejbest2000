d5a7050581aac2d755f0ed6aa3b1948833e34c2f
ARUN <arungirdhar@qainfotech.com>
2019-01-09 13:03:27 +0530

    Revert "verify"
    This reverts commit a3acd459c96680b3f312827be8b15dc653328ea4.

--- initiate.py
+++ initiate.py
@@ -2,17 +2,11 @@ import unittest
 import data as d
 import sendEmail
 from fabric import Connection
-import sys
 
 
 class CommandTests(unittest.TestCase):
     processes = []
-    server = d.server
-    username = d.username
-    password = d.password
-    command = d.command
-    msg = ""
-
+    msg =""
     def tearDown(self):
         for p in self.processes:
             if p.poll() is None:
@@ -23,27 +17,18 @@ class CommandTests(unittest.TestCase):
         :return:
         """
         try:
-            print('server', self.server)
-            print('username', self.username)
-            print('password', self.password)
-            c = Connection(self.server, user=d.username, connect_timeout=600, connect_kwargs={'password': self.password})
-            res = c.run(self.command)
+            c = Connection(d.server, user=d.username, connect_timeout=600, connect_kwargs={'password': d.password})
+            res = c.run(d.command)
             verify = c.run("echo $?")
             print(str(verify)+"---")
             self.assertEqual(verify.stdout.strip(), "0")
-            self.msg = str(res.stdout)
+            msg = str(res.stdout)
         except Exception as e:
-            self.msg=str(e.result)
-
+            msg=str(e.result)
         finally:
             c.close()
-            sendEmail.main("From Initiate : ", str(self.msg))
+            sendEmail.main("From Initiate : ",str(msg))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        CommandTests.server = sys.argv.pop()
-        CommandTests.password = sys.argv.pop()
-        CommandTests.username = sys.argv.pop()
-        CommandTests.command = sys.argv.pop()
     unittest.main()
